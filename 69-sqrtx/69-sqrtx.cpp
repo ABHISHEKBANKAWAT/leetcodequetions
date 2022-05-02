bdd33bfa69dc0e3f560d4de7ce4ce031b8e753e7
ABHISHEKBANKAWAT <50920449+ABHISHEKBANKAWAT@users.noreply.github.com>
2022-05-03 00:42:27 +0530

    Time: 23 ms (13.69%), Space: 5.9 MB (75.37%) - LeetHub

--- 69-sqrtx/69-sqrtx.cpp
+++ 69-sqrtx/69-sqrtx.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        // for(int i=0;i<=x;i++){
-        //     if(i*i>x){
-        //         return i-1;
-        //     }else if(i*i==x){
-        //         return i;
-        //     }
-        // }
-        // return -1;
-        long int s = 0 , e = 1e8 , ans = 0;
-        while(s<=e){
-            long int m = s+(e-s)/2;
-            if(m*m<=x){
-                ans = m;
-                s= m+1;
-            }
-            else{
-                e = m-1;
+        for(long int i=0;i<=x;i++){
+            if(i*i>x){
+                return i-1;
+            }else if(i*i==x){
+                return i;
             }
         }
-        return ans;
+        return -1;
     }
+    //     long int s = 0 , e = 1e8 , ans = 0;
+    //     while(s<=e){
+    //         long int m = s+(e-s)/2;
+    //         if(m*m<=x){
+    //             ans = m;
+    //             s= m+1;
+    //         }
+    //         else{
+    //             e = m-1;
+    //         }
+    //     }
+    //     return ans;
+    // }
 };
