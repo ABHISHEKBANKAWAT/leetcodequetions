5eb8bf751f3a46a77fc0ff4ac5affd69d4fc36d6
ABHISHEKBANKAWAT <50920449+ABHISHEKBANKAWAT@users.noreply.github.com>
2022-05-05 10:17:01 +0530

    Time: 4 ms (22.98%), Space: 6.7 MB (88.02%) - LeetHub

--- 225-implement-stack-using-queues/225-implement-stack-using-queues.cpp
+++ 225-implement-stack-using-queues/225-implement-stack-using-queues.cpp
@@ -1,19 +1,54 @@
+// class MyStack {
+// public:
+//     /** Initialize your data structure here. */
+//     queue<int> q1;
+//     queue<int> q2;
+//     MyStack() {
+        
+//     }
+    
+//     /** Push element x onto stack. */
+//     void push(int x) {
+//         q2.push(x);
+//         while(!q1.empty()){
+//         q2.push(q1.front());    q1.pop();
+//         }
+//         swap(q1, q2);
+//     }
+    
+//     /** Removes the element on top of the stack and returns that element. */
+//     int pop() {
+//         int result = top();
+//         q1.pop();
+//         return result;
+//     }
+    
+//     /** Get the top element. */
+//     int top() {
+//         return q1.front();
+//     }
+    
+//     /** Returns whether the stack is empty. */
+//     bool empty() {
+//         return q1.empty();
+//     }
+// };
 class MyStack {
 public:
     /** Initialize your data structure here. */
     queue<int> q1;
-    queue<int> q2;
     MyStack() {
         
     }
     
     /** Push element x onto stack. */
     void push(int x) {
-        q2.push(x);
-        while(!q1.empty()){
-            q2.push(q1.front());    q1.pop();
+        int sz = q1.size();
+        q1.push(x);
+        while(sz--){
+            q1.push(q1.front());
+            q1.pop();
         }
-        swap(q1, q2);
     }
     
     /** Removes the element on top of the stack and returns that element. */
@@ -33,6 +68,7 @@ public:
         return q1.empty();
     }
 };
+
 
 /**
  * Your MyStack object will be instantiated and called as such:
