df544d991d6c155aa5bbd35c815e0f183d002bf0
ABHISHEKBANKAWAT <50920449+ABHISHEKBANKAWAT@users.noreply.github.com>
2022-05-06 18:48:33 +0530

    Time: 12 ms (97.42%), Space: 9.8 MB (95.08%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -1,21 +1,29 @@
 class Solution {
 public:
-    string removeDuplicates(string s) {
-         stack<char> st;
-        string ans;
-        for(int i = 0 ; i< s.length();i++){
-            if(st.empty() || st.top() != s[i]){
-                st.push(s[i]);
-            }
-            else if(st.top()==s[i]){
-                st.pop();
-            }
-        }
-        while(st.size()!=0){
-        ans = st.top()+ans;
-            st.pop();
-        }
-        return ans;
+//     string removeDuplicates(string s) {
+//          stack<char> st;
+//         string ans;
+//         for(int i = 0 ; i< s.length();i++){
+//             if(st.empty() || st.top() != s[i]){
+//                 st.push(s[i]);
+//             }
+//             else if(st.top()==s[i]){
+//                 st.pop();
+//             }
+//         }
+//         while(st.size()!=0){
+//         ans = st.top()+ans;
+//             st.pop();
+//         }
+//         return ans;
     
+//     }
+// };
+    string removeDuplicates(string& s) {
+        int i = 0;
+        for(int j = 1; j < s.size(); ++j)
+            if (i < 0 || s[i] != s[j]) s[++i] = s[j];
+            else --i;
+        return s.substr(0, i + 1);
     }
 };
