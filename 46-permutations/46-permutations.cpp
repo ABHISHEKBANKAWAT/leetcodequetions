16f797a387c7eb2ccfe574b0694d3a0a9522fde1
ABHISHEKBANKAWAT <50920449+ABHISHEKBANKAWAT@users.noreply.github.com>
2022-05-09 00:33:51 +0530

    Time: 4 ms (63.48%), Space: 7.7 MB (78.84%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -1,26 +1,39 @@
 class Solution {
 public:
-    void helper(vector<int> &ds,vector<vector<int>> &ans,vector<int> &nums,int freq[]){
-        if(ds.size()==nums.size()){
-            ans.push_back(ds);
+    // void helper(vector<int> &ds,vector<vector<int>> &ans,vector<int> &nums,int freq[]){
+    //     if(ds.size()==nums.size()){
+    //         ans.push_back(ds);
+    //         return;
+    //     }
+    //     for(int i = 0 ; i < nums.size();i++){
+    //         if(!freq[i]){
+    //             ds.push_back(nums[i]);
+    //             freq[i] = 1;
+    //             helper(ds,ans,nums,freq);
+    //             freq[i] = 0;
+    //             ds.pop_back();
+    //         }
+    //     }
+    // }
+    void helper(int idx, vector<int> &nums,vector<vector<int>> &ans){
+        if(idx==nums.size()){
+            ans.push_back(nums);
             return;
         }
-        for(int i = 0 ; i < nums.size();i++){
-            if(!freq[i]){
-                ds.push_back(nums[i]);
-                freq[i] = 1;
-                helper(ds,ans,nums,freq);
-                freq[i] = 0;
-                ds.pop_back();
+        for(int i = idx ; i < nums.size();i++){
+            swap(nums[idx],nums[i]);
+            helper(idx+1,nums,ans);
+            swap(nums[idx],nums[i]);
+            
             }
-        }
     }
+    
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> ans;
-        vector<int> ds;
-        int n = nums.size();
-        int freq[10] = {0};
-        helper(ds,ans,nums,freq);
+       // vector<int> ds;
+        //int n = nums.size();
+        //int freq[10] = {0};
+        helper(0,nums,ans);
         return ans;
     }
 };
