00057c009a500ab73bc75edd6fa872dbd7bb46f6
ABHISHEKBANKAWAT <50920449+ABHISHEKBANKAWAT@users.noreply.github.com>
2022-05-11 23:29:49 +0530

    Time: 53 ms (6.19%), Space: 15 MB (48.01%) - LeetHub

--- 203-remove-linked-list-elements/203-remove-linked-list-elements.cpp
+++ 203-remove-linked-list-elements/203-remove-linked-list-elements.cpp
@@ -10,26 +10,17 @@
  */
 class Solution {
 public:
-      ListNode* removeElements(ListNode* head, int val) {
-        if(head==nullptr) return head;
-		// This is for the case when a linked list is like this: 
-		// 1->1->2->null , val = 1
-		// 1->1->1->null , val = 1
-        while(head!=nullptr && head->val==val){
-            head = head->next;
-        }
-        ListNode* curr = head;
-        while(curr!=nullptr  && curr->next!=nullptr){
-            if(curr->next->val==val){
-                curr->next = curr->next->next;
-				// After doing the above step, I am not updating "curr" because of these type of test cases:
-				// 1->2->3->6->6->6->5->null  val = 6
-            }
-            else
-                curr = curr->next;
-        }
-        return head;
+    ListNode* removeElements(ListNode* head, int T) {
+        ListNode *dummy = new ListNode(-1, head), *prev = dummy;
+        while(prev!=NULL && prev->next!=NULL){
+            if(prev->next->val == T) 
+                prev->next = prev->next->next;                       // just update prev node
+            else                                   // current node needs to be deleted
+				prev =prev->next;       // just update next of prev to next of current so that current node is removed from list
     }
+        return dummy -> next;                     
+    }
+
     //     if(head==NULL)return NULL;
     //     head->next = removeElements(head->next,val);
     //     if(head->val == val){
