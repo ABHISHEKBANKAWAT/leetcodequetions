8091b0c9995c6fc327f040d8d3c255c0d4aab294
ABHISHEKBANKAWAT <50920449+ABHISHEKBANKAWAT@users.noreply.github.com>
2022-05-12 22:53:52 +0530

    Time: 9 ms (67.44%), Space: 10.1 MB (33.16%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.cpp
+++ 47-permutations-ii/47-permutations-ii.cpp
@@ -1,33 +1,30 @@
-
 class Solution {
 private:
-    void solve(vector<int>& nums,set<vector<int>> &ans,vector<int> t){
-        if(t.size()==nums.size()){            // Base case-> if temporsry vector size becomes equal to size of given list 
-            ans.insert(t);                // Before returning store the valid permutation inside the ans
+    void solve(vector<int>& nums,vector<vector<int>> &ans,vector<int> t){
+        if(t.size()==nums.size()){
+            ans.push_back(t);
             return ;
         }
         
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]!=11){            // check if the element is already included
-                int j=nums[i];
-                nums[i]=11;        // make the current ele to 11 (which is out of the range) so that we will not insert this element in next call
-                t.push_back(j);      // store it in the temporary vector
-                solve(nums,ans,t);  //recursively call to complete the permutations
-                nums[i]=j;      // backtrack 
-                t.pop_back();
+        for(int j=0;j<nums.size();j++){
+            if(nums[j]!=11){
+			// The difference between the upper code and this code
+			
+            if(j>0 && nums[j]==nums[j-1]) continue;       // skip the element which has already been included
+            int i=nums[j];
+            nums[j]=11;
+            t.push_back(i);
+            solve(nums,ans,t);
+            t.pop_back();
+            nums[j]=i;
             }
         }
     }
 public:
     vector<vector<int>> permuteUnique(vector<int>& nums) {
-        set<vector<int>> s;          //to store unique permutations
+        sort(nums.begin(),nums.end());          // sort the givern nums vector to check the consecutive elements
         vector<vector<int>> ans;
-        
-        solve(nums,s,{});
-        
-        for(auto i:s){
-            ans.push_back(i);
-        }
+        solve(nums,ans,{});
         return ans;
     }
 };
