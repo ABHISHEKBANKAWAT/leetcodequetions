800fa39f74ab4fc4b9af2df3299da9e7278bc627
ABHISHEKBANKAWAT <50920449+ABHISHEKBANKAWAT@users.noreply.github.com>
2022-05-28 22:23:42 +0530

    Time: 28 ms (46.47%), Space: 17.9 MB (65.37%) - LeetHub

--- 268-missing-number/268-missing-number.cpp
+++ 268-missing-number/268-missing-number.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
-    int missingNumber(vector<int>& nums) {
-        int result = 0;
-        for(int i= 0 ;i<nums.size();i++){
-            result^=nums[i];
+    int missingNumber(vector<int>& nums) { // function to return missing number
+        int result = nums.size(); // initialize result to size of vector
+        int i=0; // initialize i to 0
+        for(int num:nums){ // for each number in vector
+            result ^= num; // XOR result with number
+            result ^= i; // XOR result with i
+            i++; // increment i
         }
-        for(int i=0;i<=nums.size();i++){
-            result^=i;
-        }
-        return result;
+        return result; // return result that is the missing number
     }
 };
